0bed3f65fc5e8dc646536dfd59163a5713b7ca96
Mr-rabbit-xd <bihh5539@gmail.com>
2025-10-08 14:55:15 +0530

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -1,20 +1,34 @@
+const axios = require("axios");
 const { plugin, mode } = require("../lib");
 
 plugin(
   {
     pattern: "pair ?(.*)",
+    desc: "Generate WhatsApp Pair Code",
+    react: "🔐",
     fromMe: mode,
-    desc: "Get WhatsApp pairing code",
-    type: "tools",
+    type: "system",
   },
   async (message, match) => {
     try {
-      await message.send(
-        "*Not available.*\n\n👉 Please visit: https://y.vercel.app"
-      );
-    } catch (error) {
-      console.error(error);
-      await message.send("An error occurred. Please try again later.");
+      const number = match?.trim();
+      if (!number)
+        return await message.reply("⚠️ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞: *.pair 919876543210* 💋");
+
+      const waitMsg = await message.reply("⏳ 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐢𝐧𝐠... 😚");
+
+      const api = `https://mainline.proxy.rlwy.net:55620/pair?code=${number}`;
+      const { data } = await axios.get(api);
+
+      if (data?.status === "success" && data?.code) {
+        await message.client.sendMessage(message.jid, {
+          text: `🔑 ${data.code}`,
+        });
+      } else {
+        await message.reply("💔 𝐅𝐚𝐢𝐥𝐞𝐝 𝐓𝐫𝐲 𝐀𝐠𝐚𝐢𝐧 😋");
+      }
+    } catch {
+      await message.reply("⚠️ 𝐒𝐞𝐫𝐯𝐞𝐫 𝐄𝐫𝐫𝐨𝐫 😚");
     }
   }
 );
